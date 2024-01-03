c5d449654f84ad5cb88f399fe9cb097c3f277d62
Sachinthana Piyumal <145235176+Soap171@users.noreply.github.com>
2024-01-04 00:09:43 +0530

    hero component created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,12 @@
 import Navbar from "../../components/navbar/Navbar";
+import Hero from "../../components/hero-section/Hero";
+import Card from "../../components/card-section/Card";
+
 function Home() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
