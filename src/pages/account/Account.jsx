00d98ee8415d6ab5117820e9775706282c02dc6d
Sachinthana Piyumal <145235176+Soap171@users.noreply.github.com>
2024-01-05 14:58:11 +0530

    fixed some issues within account page

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -1,65 +1,81 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
-import AccountImg from "../../img/Account.jpg";
+import AccountImg from "../../img/Logo.png";
 import "./Account.css";
 
 function Account() {
   return (
-    <div className="bg">
+    <>
       <Navbar />
-      <div className="container-fluid content min-vh-100 ">
-        <div className="row justify-content-center align-items-center my-4 d-flex vh-100 ">
-          <div className="col-12 col-md-4">
-            <img src={AccountImg} alt="" className="img-fluid rounded-4" />
-          </div>
-          <div className="col-12 col-md-8 my-4">
-            <form action="" className="row g-3">
-              <div class="col-md-6">
-                <label for="Email" class="form-label">
-                  Email
-                </label>
-                <input type="email" class="form-control" id="Email" />
-              </div>
+      <div className="bg container-fluid">
+        <div className="content min-vh-100 ">
+          <div className="row justify-content-center align-items-center my-4 d-flex vh-100 ">
+            <div className="col-12 col-md-4 text-center">
+              <img
+                src={AccountImg}
+                alt=""
+                className="img-fluid rounded-5 ps-3"
+              />
+            </div>
+            <div className="col-12 col-md-8 my-4">
+              <form action="" className="row g-3">
+                <div class="form-floating mb-3 col-md-6">
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="email"
+                    placeholder=""
+                  />
+                  <label for="email">Email address</label>
+                </div>
 
-              <div class="col-12">
-                <label for="Address" class="form-label">
-                  Address
-                </label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="Address"
-                  placeholder="1234 Main St"
-                />
-              </div>
+                <div class="form-floating mb-3 col-md-6">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="OwnerName"
+                    placeholder=""
+                  />
+                  <label for="OwnerName">Owner Name</label>
+                </div>
 
-              <div class="col-md-6">
-                <label for="OwnerName" class="form-label">
-                  Owner Name
-                </label>
-                <input type="text" class="form-control" id="OwnerName" />
-              </div>
-              <div class="col-md-4">
-                <label for="City" class="form-label">
-                  City
-                </label>
-                <input type="text" class="form-control" id="City" />
-              </div>
-              <div class="col-12">
-                <button type="submit" class="btn btn-primary">
-                  Save
-                </button>
-                <button type="submit" class="btn btn-secondary ms-2">
-                  Edit
-                </button>
-              </div>
-            </form>
+                <div class="form-floating mb-3 col-md-8">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="Address"
+                    placeholder=""
+                  />
+                  <label for="Address">Address</label>
+                </div>
+
+                <div class="form-floating mb-3 col-md-4">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="City"
+                    placeholder=""
+                  />
+                  <label for="City" class="form-label">
+                    City
+                  </label>
+                </div>
+                <div class="col-12">
+                  <button type="submit" class="btn btn-primary">
+                    Save
+                  </button>
+                  <button type="submit" class="btn btn-secondary ms-2">
+                    Edit
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 }
 
