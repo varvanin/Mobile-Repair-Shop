99f49087871c0eb5c3560052a369ebe8b6a2da45
Sachinthana Piyumal <145235176+Soap171@users.noreply.github.com>
2024-01-05 15:05:54 +0530

    fixed layout of the account page

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -6,76 +6,70 @@ import "./Account.css";
 
 function Account() {
   return (
-    <>
+    <div className="bg container-fluid d-flex flex-column min-vh-100">
       <Navbar />
-      <div className="bg container-fluid">
-        <div className="content min-vh-100 ">
-          <div className="row justify-content-center align-items-center my-4 d-flex vh-100 ">
-            <div className="col-12 col-md-4 text-center">
-              <img
-                src={AccountImg}
-                alt=""
-                className="img-fluid rounded-5 ps-3"
-              />
-            </div>
-            <div className="col-12 col-md-8 my-4">
-              <form action="" className="row g-3">
-                <div class="form-floating mb-3 col-md-6">
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="email"
-                    placeholder=""
-                  />
-                  <label for="email">Email address</label>
-                </div>
+      <div className="content flex-grow-1">
+        <div className="row justify-content-center align-items-center my-4">
+          <div className="col-12 col-md-4 text-center">
+            <img src={AccountImg} alt="" className="img-fluid rounded-5 ps-3" />
+          </div>
+          <div className="col-12 col-md-8 my-4">
+            <form action="" className="row g-3">
+              <div className="form-floating mb-3 col-md-6">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder=""
+                />
+                <label htmlFor="email">Email address</label>
+              </div>
 
-                <div class="form-floating mb-3 col-md-6">
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="OwnerName"
-                    placeholder=""
-                  />
-                  <label for="OwnerName">Owner Name</label>
-                </div>
+              <div className="form-floating mb-3 col-md-6">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="OwnerName"
+                  placeholder=""
+                />
+                <label htmlFor="OwnerName">Owner Name</label>
+              </div>
 
-                <div class="form-floating mb-3 col-md-8">
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="Address"
-                    placeholder=""
-                  />
-                  <label for="Address">Address</label>
-                </div>
+              <div className="form-floating mb-3 col-md-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="Address"
+                  placeholder=""
+                />
+                <label htmlFor="Address">Address</label>
+              </div>
 
-                <div class="form-floating mb-3 col-md-4">
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="City"
-                    placeholder=""
-                  />
-                  <label for="City" class="form-label">
-                    City
-                  </label>
-                </div>
-                <div class="col-12">
-                  <button type="submit" class="btn btn-primary">
-                    Save
-                  </button>
-                  <button type="submit" class="btn btn-secondary ms-2">
-                    Edit
-                  </button>
-                </div>
-              </form>
-            </div>
+              <div className="form-floating mb-3 col-md-4">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="City"
+                  placeholder=""
+                />
+                <label htmlFor="City" className="form-label">
+                  City
+                </label>
+              </div>
+              <div className="col-12">
+                <button type="submit" className="btn btn-primary">
+                  Save
+                </button>
+                <button type="submit" className="btn btn-secondary ms-2">
+                  Edit
+                </button>
+              </div>
+            </form>
           </div>
         </div>
-        <Footer />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
 
